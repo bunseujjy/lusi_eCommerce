dbda2cc5a5e7f0f534821c97f5ba99be22e11964
Bunseu <bunseueng@gmail.com>
2024-04-03 17:23:20 +0700

    changing webhook

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,34 +1,47 @@
-import Stripe from "stripe"
-import prisma from "@/lib/db"
-import { NextResponse } from "next/server"
-import { headers } from "next/headers"
+import Stripe from "stripe";
+import prisma from "@/lib/db";
+import { NextResponse } from "next/server";
+import { headers } from "next/headers";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export async function POST(request: Request): Promise<any> {
-    const text = await request.json()
-    const stripe = new Stripe(`${process.env.STRIPE_SECRET_KEY}`)
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '1mb',
+    },
+  },
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const text = await req.body();
+    const stripe = new Stripe(`${process.env.STRIPE_SECRET_KEY}`);
     const signature = headers().get("stripe-signature") ?? "";
-    let event: Stripe.Event
+    let event: Stripe.Event;
 
     try {
-        event = stripe.webhooks.constructEvent(text, signature, process.env.STRIPE_WEBHOOK_SECRET!)
+      event = stripe.webhooks.constructEvent(text, signature, process.env.STRIPE_WEBHOOK_SECRET!);
 
-        if(event?.type === "checkout.session.completed") {
-            const metadata = event.data?.object?.metadata
-            const paymentStatus = event.data?.object?.payment_status
-            if(metadata?.orderId && paymentStatus === "paid") {
-                const order = await prisma.order.update({
-                    where: {
-                        id: metadata.orderId
-                    },
-                    data: {
-                        paid: "paid"
-                    }
-                })
-                return NextResponse.json({order,message: "Order updated successfully"}, {status: 200})
+      if (event?.type === "checkout.session.completed") {
+        const metadata = event.data?.object?.metadata;
+        const paymentStatus = event.data?.object?.payment_status;
+        if (metadata?.orderId && paymentStatus === "paid") {
+          const order = await prisma.order.update({
+            where: {
+              id: metadata.orderId
+            },
+            data: {
+              paid: "paid"
             }
+          });
+          return NextResponse.json({ order, message: "Order updated successfully" }, { status: 200 });
         }
-        return NextResponse.json({message: "Payment paid successfully"}, {status: 201})
+      }
+      return NextResponse.json({ message: "Payment paid successfully" }, { status: 200 });
     } catch (error) {
-        return NextResponse.json({error: error}, {status: 500})
+      return NextResponse.json({ error: error }, { status: 500 });
     }
+  } else {
+    console.log("Error")
+  }
 }
